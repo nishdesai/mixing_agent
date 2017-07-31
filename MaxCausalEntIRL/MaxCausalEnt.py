16e282ac89ed4eceb99feb0887840c156a1fb0b9
Dmitrii Krasheninnikov <dmkr0001@gmail.com>
2017-07-31 15:14:29 -0700

    simpler functions, better variable names, docs

--- MaxCausalEntIRL/MaxCausalEnt.py
+++ MaxCausalEntIRL/MaxCausalEnt.py
@@ -1,6 +1,5 @@
 from frozen_lake import *
 import numpy as np, numpy.random as nr, gym
-import matplotlib.pyplot as plt
 from gym.spaces import prng
 
 class MDP(object):
@@ -70,30 +69,29 @@ def compute_value_boltzmann(mdp, gamma, r, horizon = None, threshold=1e-4):
     
     V = np.zeros(mdp.nS)
     
-    # Running into numerical problems with r= [[0]*63, 1] for some reason
+    # Running into numerical problems with r= [[0]*63, 1] for some reason. Critch, check this out?
     V = r * .99999
 
     t = 0
     diff = float("inf")
     while diff > threshold:
-        #print(t, V)
         V_prev = np.copy(V)
-        diff = 0
         for s in range(mdp.nS):
             # V_s_new = \log[  \sum_a \exp(  r_s + \gamma \sum_{s'} p(s'|s,a)V_{s'}   )  ]
             for a in range(mdp.nA):
+                # If-else statement is used to compute softmax correctly. If V[s] is initialized as 0 and
+                # only the expression from 'else' is used, there would be an additional e^0 in the sum.
                 if a == 0:
-                    V_s_new = r[s] + gamma * np.dot(mdp.T[s, a, :], V_prev)  
+                    # V[s] = r_s + \gamma \sum_{s'} p(s'|s,a)V_{s'}
+                    V[s] = r[s] + gamma * np.dot(mdp.T[s, a, :], V_prev)  
                 else:
-                    V_s_new = softmax(V_s_new, r[s] + gamma * np.dot(mdp.T[s, a, :], V_prev))
+                    # V[s] = log(   exp(V[s]) + exp(r_s + \gamma \sum_{s'} p(s'|s,a)V_{s'})   )
+                    V[s] = softmax(V[s], r[s] + gamma * np.dot(mdp.T[s, a, :], V_prev))
             
-                if np.sum(np.isnan(V_s_new)) > 0: 
+                if np.sum(np.isnan(V[s])) > 0: 
                     raise Exception('NaN encountered in value, iteration ', t, 'state',s, ' action ', a)
-            
-            V[s] = V_s_new
-            
-        new_diff = np.amax(abs(V_prev - V))
-        if new_diff > diff: diff = new_diff
+                        
+        diff = np.amax(abs(V_prev - V))
         
         t+=1
         if horizon is not None:
@@ -167,37 +165,44 @@ def compute_s_a_visitations(mdp, gamma, trajectories):
     the expert trajectories
     
     Empirical state-action visitation frequencies:
-    sa_visit_count = \sum_{i,t} 1_{s_{i,t} = s \wedge a_{i,t} = a}
+    sa_visit_count[s,a] = \sum_{i,t} 1_{traj_s_{i,t} = s \wedge traj_a_{i,t} = a}
 
     P_0(s) for initialization of the algorithm computing the occupancy measure 
     of a MDP under a given policy:
-    init_s[j] = \sum_a sa_visit_count[j] - \sum_{i,t} \gamma P(s_j | s_it, a_it)
+    P_0[s] = \sum_{i,t} 1_{t = 0 \wedge traj_s_{i,t} = s}  / i
+
+    Parameters
+    ----------
+    mdp : object
+        Instance of the MDP class.
+    gamma : float 
+        Discount factor; 0<=gamma<=1.
+    trajectories : 3D numpy array
+        Expert trajectories. 
+        Dimensions: [number of trajectories, timesteps in the trajectory, state and action].
+
+    Returns
+    -------
+    (2D numpy array, 1D numpy array)
+        Arrays of shape (mdp.nS, mdp.nA) and (mdp.nS).
     """
-    
+
+    s_0_count = np.zeros(mdp.nS)
     sa_visit_count = np.zeros((mdp.nS, mdp.nA))
-    init_s = np.zeros((mdp.nS))
+    
     for traj in trajectories:
+        # traj[0][0] is the state of the first timestep of the trajectory.
+        s_0_count[traj[0][0]] += 1
         for (s, a) in traj:
             sa_visit_count[s, a] += 1
-            init_s[s] += 1
-
-            # This is the - \gamma P(s_j | s_it, a_it) term.
-            init_s -= gamma * mdp.T[s,a,:]
-            # Same as the line above but slower:
-            #for (s_prime, p_transition) in enumerate(t[s,a,:]):
-            #    init_s[s_prime] -= gamma * p_transition
+      
+    # Count into probability        
+    P_0 = s_0_count / trajectories.shape[0]
     
-    init_s = init_s / (np.sum(init_s))
-    #init_s = init_s / (trajectories.shape[0] * trajectories.shape[1])
-    #sa_visit_count = sa_visit_count / (trajectories.shape[0] * trajectories.shape[1])
-    
-
-    if np.sum(np.isnan(sa_visit_count)) > 0: raise Exception('NaN encountered')
-    
-    return(sa_visit_count, init_s)
+    return(sa_visit_count, P_0)
 
 
-def compute_D(mdp, gamma, V, policy, init_s, horizon=None, D = None, threshold = 1e-6):
+def compute_D(mdp, gamma, V, policy, init_s=None, horizon=None, D_prev = None, threshold = 1e-6):
     """
     Computes occupancy measure of a MDP under a given policy -- 
     the expected discounted number of times that policy π visits state s.
@@ -209,39 +214,32 @@ def compute_D(mdp, gamma, V, policy, init_s, horizon=None, D = None, threshold =
     
     assert np.sum(np.isnan(V)) == 0
     assert np.sum(np.isnan(policy)) == 0
-    assert np.sum(np.isnan(init_s)) == 0
         
-    
-    if D is None: D = np.ones((mdp.nS)) / mdp.nS     
+    if init_s is None: init_s = np.ones(mdp.nS) / mdp.nS
+    if D_prev is None: D_prev = np.copy(init_s)     
     
     t = 1
     
+
     diff = float("inf")
     while diff > threshold:
         
-        #D_new = np.zeros_like(D)
-        D_new = init_s
+        D = np.copy(init_s)
 
         for s in range(mdp.nS):
             for a in range(mdp.nA):
                 for p_sprime, s_prime, _ in mdp.P[s][a]:
 
-                    D_new[s_prime] += (policy[s, a] * ( gamma * p_sprime * D[s]))
+                    D[s_prime] += (policy[s, a] * ( gamma * p_sprime * D_prev[s]))
 
-            if np.sum(D_new>1e10) > 0: raise Warning('D_new > 1e10, iteration', t)
-        
-        # Normalize for the frequency interpretation. Not sure if it is correct to do this,
-        # but otherwise D essentially doubles at each iteration and never converges.
-        D_new = D_new / np.sum(D_new)
-        
-        diff = np.amax(abs(D - D_new))    
-        D = np.copy(D_new)
+        diff = np.amax(abs(D_prev - D))    
+        D_prev = np.copy(D)
         
         if horizon is not None:
             t+=1
             if t==horizon: break
     
-    if np.sum(np.isnan(D)) > 0: raise Exception('NaN encountered in occupancy measure')
+    if np.sum(np.isnan(D_prev)) > 0: raise Exception('NaN encountered in occupancy measure')
     return D
 
 
@@ -276,44 +274,41 @@ def max_causal_ent_irl(mdp, gamma, trajectories, epochs=1, learning_rate=0.2, r 
         Reward vector computed with Maximum Causal Entropy algorithm from the expert trajectories.
     """
 
-    if r is not None:
-        r = np.random.rand(mdp.nS)
+    
 
     sa_visit_count, init_s = compute_s_a_visitations(mdp, gamma, trajectories)
     
+    if r is not None:
+        r = np.random.rand(mdp.nS)
+
     for i in range(epochs):
         V = compute_value_boltzmann(mdp, gamma, r, horizon=horizon)
         policy = compute_policy(mdp, gamma, r=r, V=V) 
         
         # IRL log likelihood term
-        L_D = np.sum(sa_visit_count * np.log(policy))
+        L = np.sum(sa_visit_count * np.log(policy))
         
         # IRL log likelihood gradient w.r.t reward, inverted for descent
-        D = compute_D(mdp, gamma, V, policy, init_s, horizon=horizon)        
-        
-        #TODO: figure out E_f
-        E_f = np.zeros(mdp.nS)
-        for traj in trajectories: 
-            for (s,a) in traj: 
-                E_f += D[s]
+        D = compute_D(mdp, gamma, V, policy, init_s, horizon=trajectories.shape[1])        
 
-        dL_D_dr = -(np.sum(sa_visit_count,1) - D)
+        # Minus sign to get the gradient of negative log likelihood, which we then minimize wiht GD
+        dL_dr = -(np.sum(sa_visit_count,1) / trajectories.shape[0] - D)
 
         # Gradient descent
-        r = r - learning_rate * dL_D_dr
+        r = r - learning_rate * dL_dr
 
-        print('Epoch: ',i, 'log likelihood of the trajectories: ', L_D)
+        print('Epoch: ',i, 'log likelihood of all traj: ', L, 'average per traj step: ', L/(trajectories.shape[0] * trajectories.shape[1]))
 
     return r
 
 
 def main():
 
-    learning_rate = 0.0001
+    learning_rate = 0.1
     epochs = 20
     
-    gamma = 0.99
-    horizon = 500
+    gamma = 1
+    horizon = 200
     traj_len = 15
 
     env = FrozenLakeEnvMultigoal(goal=2)
@@ -327,9 +322,12 @@ def main():
     trajectories1 = generate_trajectories(mdp1, policy1, T=traj_len, num_traj=200)
     print('Generated ', trajectories1.shape[0],' trajectories of length ', traj_len)
 
+    sa_visit_count, _ = compute_s_a_visitations(mdp1, gamma, trajectories1)
+    print('Log likelihood of the trajectories under the policy generated from the original reward: ', np.sum(sa_visit_count * np.log(policy1)), 
+        'average per traj step: ', np.sum(sa_visit_count * np.log(policy1)) / (trajectories1.shape[0] * trajectories1.shape[1]), '\n' )
+
     r = np.random.rand(mdp1.nS)
-    #r[-1] = 1
-    print('Initial reward: ',r)
+    print('Randomly initialized reward: ',r)
 
     r = max_causal_ent_irl(mdp=mdp1, gamma=gamma, trajectories=trajectories1, 
                         epochs=epochs, learning_rate=learning_rate, r = r, horizon=horizon)
