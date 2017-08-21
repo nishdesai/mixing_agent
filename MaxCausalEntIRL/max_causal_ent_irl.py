b80056ccd62cdb7aeb6a084c9be0c4dc98f5df08
Dmitrii Krasheninnikov <dmkr0001@gmail.com>
2017-08-21 13:17:53 -0700

    separate files

--- MaxCausalEntIRL/max_causal_ent_irl.py
+++ MaxCausalEntIRL/max_causal_ent_irl.py
@@ -1,8 +1,10 @@
 import numpy as np 
 from frozen_lake import FrozenLakeEnvMultigoal
-from mdps import MDP, MDPOneTimeR, generate_trajectories
+from mdps import MDP, MDPOneTimeR
+from traj_tools import generate_trajectories, compute_s_a_visitations
 from value_iter_and_policy import vi_boltzmann, compute_policy_boltzmann 
 from value_iter_and_policy import vi_rational, compute_policy_rational
+from occupancy_measure import compute_D
 
 def max_causal_ent_irl(mdp, trajectories, gamma=1, horizon=None, temperature=1, 
                        epochs=1, learning_rate=0.2, r=None):
@@ -85,111 +87,13 @@ def max_causal_ent_irl(mdp, trajectories, gamma=1, horizon=None, temperature=1,
     return r
 
 
-def compute_s_a_visitations(mdp, gamma, trajectories):
-    '''
-    Computes the state-action visitation counts and the probability 
-    of a trajectory starting in state s from the expert trajectories.
-    
-    State-action visitation counts:
-    sa_visit_count[s,a] = \sum_{i,t} 1_{traj_s_{i,t} = s AND traj_a_{i,t} = a}
-
-    P_0(s) -- probability that the trajectory will start in state s. 
-    P_0[s] = \sum_{i,t} 1_{t = 0 AND traj_s_{i,t} = s}  / i
-    Used in computing the occupancy measure of a MDP under a given policy.
-
-    Parameters
-    ----------
-    mdp : object
-        Instance of the MDP class.
-    gamma : float 
-        Discount factor; 0<=gamma<=1.
-    trajectories : 3D numpy array
-        Expert trajectories. 
-        Dimensions: [number of traj, timesteps in the traj, state and action].
-
-    Returns
-    -------
-    (2D numpy array, 1D numpy array)
-        Arrays of shape (mdp.nS, mdp.nA) and (mdp.nS).
-    '''
-
-    s_0_count = np.zeros(mdp.nS)
-    sa_visit_count = np.zeros((mdp.nS, mdp.nA))
-    
-    for traj in trajectories:
-        # traj[0][0] is the state of the first timestep of the trajectory.
-        s_0_count[traj[0][0]] += 1
-        for (s, a) in traj:
-            sa_visit_count[s, a] += 1
-      
-    # Count into probability        
-    P_0 = s_0_count / trajectories.shape[0]
-    
-    return sa_visit_count, P_0
-
-
-def compute_D(mdp, gamma, policy, P_0=None, t_max=None, threshold=1e-6):
-    '''
-    Computes occupancy measure of a MDP under a given time-constrained policy 
-    -- the expected number of times that policy π visits state s in a given 
-    number of timesteps.
-    
-    Described in Algorithm 9.3 of Ziebart's PhD thesis 
-    http://www.cs.cmu.edu/~bziebart/publications/thesis-bziebart.pdf.
-
-    Parameters
-    ----------
-    mdp : object
-        Instance of the MDP class.
-    gamma : float 
-        Discount factor; 0<=gamma<=1.
-    policy : 2D numpy array
-        policy[s,a] is the probability of taking action a in state s.
-    P_0 : 1D numpy array of shape (mdp.nS)
-        i-th element is the probability that the traj will start in state i.
-    t_max : int
-        number of timesteps the policy is executed.
-
-    Returns
-    -------
-    1D numpy array of shape (mdp.nS)
-    '''
-
-    if P_0 is None: P_0 = np.ones(mdp.nS) / mdp.nS
-    D_prev = np.copy(P_0)     
-    
-    t = 1
-    diff = float("inf")
-    while diff > threshold:
-        
-        # for all s: D[s] <- P_0[s]
-        D = np.copy(P_0)
-
-        for s in range(mdp.nS):
-            for a in range(mdp.nA):
-                # for all s_prime reachable from s by taking a do:
-                for p_sprime, s_prime, _ in mdp.P[s][a]:
-                    D[s_prime] += D_prev[s] * policy[s, a] * p_sprime
-
-        diff = np.amax(abs(D_prev - D))    
-        D_prev = np.copy(D)
-        
-        if t_max is not None:
-            t+=1
-            if t==t_max: break
-    
-    if np.sum(np.isnan(D_prev)) > 0: 
-        raise Exception('NaN encountered in occupancy measure')
-    return D
-
-
-def main(t_expert=1e-6,
-         t_irl=1e-6,
+def main(t_expert=1e-5,
+         t_irl=1e-5,
          gamma=1,
          horizon=10,
          n_traj=200,
          traj_len=10,
-         learning_rate=0.05,
+         learning_rate=0.01,
          epochs=31):
     '''
     Demonstrates the usage of the implemented MaxCausalEnt IRL algorithm. 
@@ -240,7 +144,7 @@ def main(t_expert=1e-6,
     # Generate expert trajectories using the given expert policy.
     trajectories = generate_trajectories(mdp, policy_expert, traj_len, n_traj)
     
-    # Compute and print various stats of the generated expert trajectories.
+    # Compute and print the stats of the generated expert trajectories.
     sa_visit_count, _ = compute_s_a_visitations(mdp, gamma, trajectories)
     log_likelihood = np.sum(sa_visit_count * (Q - V))
     print('Generated {} traj of length {}'.format(n_traj, traj_len))
