b2b59a24486ada8d29eea6e72b4279fe7c704340
Dmitrii Krasheninnikov <dmkr0001@gmail.com>
2017-08-27 00:52:58 -0700

    simpler mellowmax

--- MaxCausalEntIRL/value_iter_and_policy.py
+++ MaxCausalEntIRL/value_iter_and_policy.py
@@ -46,13 +46,13 @@ def vi_boltzmann(mdp, gamma, r, horizon=None,  temperature=1,
     while diff > threshold:
         V_prev = np.copy(V)
         
-        # Q[s,a] = (r_s + gamma * \sum_{s'} p(s'|s,a)V_{s'})
+        # ∀ s,a: Q[s,a] = (r_s + gamma * \sum_{s'} p(s'|s,a)V_{s'})
         Q = r.reshape((-1,1)) + gamma * np.dot(mdp.T, V_prev)
         if use_mellowmax:
-            # V_s = log(\sum_a exp(Q_sa) / nA)
+            # ∀ s: V_s = temperature * log(\sum_a exp(Q_{sa}/temperature) / nA)
             V = mellowmax(Q, temperature)
         else:
-            # V_s = log(\sum_a exp(Q_sa))
+            # ∀ s: V_s = temperature * log(\sum_a exp(Q_sa/temperature))
             V = softmax(Q, temperature)
 
         diff = np.amax(abs(V_prev - V))
@@ -75,10 +75,10 @@ def vi_boltzmann(mdp, gamma, r, horizon=None,  temperature=1,
     tlog = lambda x: temperature * np.log(x)
 
     if use_mellowmax:
-        # exp((Q_{s,a} - V_s - t*log(nA))/t)
+        # ∀ s,a: policy_{s,a} = exp((Q_{s,a} - V_s - t*log(nA))/t)
         policy = expt(Q - V - tlog(Q.shape[1]))
     else:
-        # exp((Q_{s,a} - V_s)/t)
+        # ∀ s,a: policy_{s,a} = exp((Q_{s,a} - V_s)/t)
         policy = expt(Q - V)
         
     return V, Q, policy
@@ -141,7 +141,7 @@ def vi_rational(mdp, gamma, r, horizon=None, threshold=1e-16):
 
     # Compute policy
     # Assigns equal probability to taking actions whose Q_sa == max_a(Q_sa)
-    max_Q_index = (Q == np.tile(np.amax(Q,axis=1),(Q.shape[1],1)).T)
+    max_Q_index = (Q == np.tile(np.amax(Q,axis=1),(mdp.nA,1)).T)
     policy = max_Q_index / np.sum(max_Q_index, axis=1).reshape((-1,1))
 
     return V, Q, policy
@@ -149,7 +149,23 @@ def vi_rational(mdp, gamma, r, horizon=None, threshold=1e-16):
 
 def softmax(x, t=1):
     '''
-    Numerically stable computation of t*log(\sum_i^n exp(x_i / t))
+    Numerically stable computation of t*log(\sum_j^n exp(x_j / t))
+    
+    If the input is a 1D numpy array, computes it's softmax: 
+        output = t*log(\sum_j^n exp(x_j / t)).
+    If the input is a 2D numpy array, computes the softmax of each of the rows:
+        output_i = t*log(\sum_j^n exp(x_{ij} / t))
+    
+    Parameters
+    ----------
+    x : 1D or 2D numpy array
+        
+    Returns
+    -------
+    1D numpy array 
+        shape = (n,), where: 
+            n = 1 if x was 1D, or 
+            n is the number of rows (=x.shape[0]) if x was 2D.
     '''
 
     if len(x.shape) == 1: x = x.reshape((1,-1))
@@ -158,9 +174,17 @@ def softmax(x, t=1):
    
     def softmax_2_arg(x1,x2, t):
         ''' 
-        Numerically stable computation of log(exp(x1) + exp(x2))
-        described in Algorithm 9.2 of Ziebart's PhD thesis 
-        http://www.cs.cmu.edu/~bziebart/publications/thesis-bziebart.pdf.
+        Numerically stable computation of t*log(exp(x1/t) + exp(x2/t))
+        
+        Parameters
+        ----------
+        x1 : numpy array of shape (n,1)
+        x2 : numpy array of shape (n,1)
+        
+        Returns
+        -------
+        numpy array of shape (n,1)
+            Each output_i = t*log(exp(x1_i / t) + exp(x2_i / t))
         '''
         tlog = lambda x: t * np.log(x)
         expt = lambda x: np.exp(x/t)
@@ -184,27 +208,5 @@ def mellowmax(x, t=1):
     Numerically stable computation of t*log(1/n \sum_i^n exp(x_i/t))
     '''
     if len(x.shape) == 1: x = x.reshape((1,-1))
-    if t == 0: return np.amax(x, axis=1)
-    if x.shape[1] == 1: return x
-    
-    tlog = lambda x: t * np.log(x)
-    expt = lambda x: np.exp(x/t)
-    
-    def mellowmax_2_arg(x1,x2, i=1):
-        ''' 
-        Numerically stable computation of 
-        log((exp(x1)/i + exp(x2))/(i+1))
-        '''
-        x1 += -tlog(1/i)
-        max_x = np.amax((x1,x2),axis=0)
-        min_x = np.amin((x1,x2),axis=0)    
-        return max_x + tlog(1+expt((min_x - max_x))) + tlog(1/(i+1))
-    
-    # Use the following property of softmax_2_arg:
-    # softmax_2_arg(softmax_2_arg(x1,x2),x3) = log(exp(x1) + exp(x2) + exp(x3))
-    # which is true since
-    # log(exp(log(exp(x1) + exp(x2))) + exp(x3)) = log(exp(x1) + exp(x2) + exp(x3))
-    mm = mellowmax_2_arg(x[:,0],x[:,1])
-    for (i, x_i) in enumerate(x.T):
-        if i>1: mm = mellowmax_2_arg(mm, x_i, i)
-    return mm
+    sm = softmax(x, t=t)
+    return sm - t*np.log(x.shape[1])
